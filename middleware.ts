da8926cd9518a839be57ec350af70e29d6116b93
Mostafa Elnemer <glpat-zSoRAujQxyQYP-AJ97FU>
2026-05-10 12:41:46 +0300

    - make MAINTENANCE mode

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 // middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-
 export function middleware(request: NextRequest) {
     const maintenanceMode =
         process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true'
